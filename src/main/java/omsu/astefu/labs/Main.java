92ff2d19fe5c089c738e0986e6df80354e4ecf5c
Kvostenko Vaycheslav <astefuapps@gmail.com>
2020-03-03 21:35:59 +0600

    add task2

--- src/main/java/omsu/astefu/labs/Main.java
+++ src/main/java/omsu/astefu/labs/Main.java
@@ -1,5 +1,7 @@
 package omsu.astefu.labs;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
 
 public class Main {
@@ -17,7 +19,7 @@ public class Main {
 
     private static int updateMenu(final Reader reader, final Menu menu) {
         menu.print();
-        int element = reader.getNumber(num -> num < 0 || num >= menu.getAmountItems(),
+        int element = reader.readNumber(num -> num < 0 || num >= menu.getAmountItems(),
                 "Enter menu: ",
                 String.format("Enter menu from 0 to %d: ", menu.getAmountItems() - 1));
         menu.accept(element);
@@ -27,20 +29,53 @@ public class Main {
     private static Menu setupMenu(final Reader reader) {
         Menu menu = new Menu();
 
-        menu.addMenuItem("Exit", () -> {
-            System.out.println("Exiting...");
-        });
-
+        menu.addMenuItem("Exit", () -> System.out.println("Exiting..."));
         menu.addMenuItem("Hello, world!", () -> System.out.println("\n\tHello, world!\n"));
-
-        menu.addMenuItem("Calc Sqrt(X) - 6 / Z", () -> {
-            int x = reader.getNumber(num -> num < 0, "\n\tEnter x: ", "\tEnter positive number: ");
-            int z = reader.getNumber(num -> num == 0, "\n\tEnter z: ", "\tEnter not zero: ");
-            double result = Math.sqrt(x) - 6.0 / z;
-            System.out.println(String.format("\n\tSqrt(%d) - 6 / %d = %.3f\n", x, z, result));
-        });
+        menu.addMenuItem("Calc Sqrt(X) - 6 / Z", printFormula(reader));
+        menu.addMenuItem("Recursion date", recursionDate(reader, 2));
 
         return menu;
+    }
+
+    // TASK 1
+    private static MenuElement printFormula(final Reader reader) {
+        return () -> {
+            int x = reader.readNumber(num -> num < 0, "\n\tEnter x: ", "\tEnter positive number: ");
+            int z = reader.readNumber(num -> num == 0, "\n\tEnter z: ", "\tEnter not zero: ");
+            double result = calculateFormula(x, z);
+            System.out.println(String.format("\n\tSqrt(%d) - 6 / %d = %.3f\n", x, z, result));
+        };
+    }
+
+    private static double calculateFormula(int x, int z) {
+        return Math.sqrt(x) - 6.0 / z;
+    }
+
+
+    // TASK 2
+    private static MenuElement recursionDate(final Reader reader, final int amount) {
+        return () -> {
+            for (int i = 0; i < amount; i++) {
+                System.out.println(String.format("\n\tCut %d:", i));
+                long days = reader.getTimeCut(reader);
+                System.out.println(printFactorial(days));
+            }
+        };
+    }
+
+    private static String printFactorial(long cut) {
+        try {
+            long fact = factorial(cut);
+            return String.format("\n\tFactorial %d! = %d\n", cut, fact);
+        } catch (IllegalArgumentException ex) {
+            return "\t" + ex.getMessage();
+        }
+    }
+
+    private static long factorial(long n) {
+        if (n < 0 || n > 30)
+            throw new IllegalArgumentException(n < 0 ? "Argument less than zero" : "Argument is sooo big");
+        return n == 0 || n == 1 ? 1 : n * factorial(n - 1);
     }
 
 
--- src/main/java/omsu/astefu/labs/Reader.java
+++ src/main/java/omsu/astefu/labs/Reader.java
@@ -1,5 +1,11 @@
 package omsu.astefu.labs;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.ChronoUnit;
 import java.util.Scanner;
 import java.util.function.Predicate;
 
@@ -11,7 +17,7 @@ public class Reader {
         this.scanner = scanner;
     }
 
-    public int getNumber() {
+    public int readNumber() {
         while (!scanner.hasNextInt()) {
             scanner.next();
             System.out.print("Try again, enter number: ");
@@ -19,17 +25,48 @@ public class Reader {
         return scanner.nextInt();
     }
 
-    public int getNumber(final String message) {
+    public int readNumber(final String message) {
         System.out.print(message);
-        return getNumber();
+        return readNumber();
     }
 
-    public int getNumber(final Predicate<Integer> predicate, final String startMessage, final String errorMessage) {
+    public int readNumber(final Predicate<Integer> predicate, final String startMessage, final String errorMessage) {
         int x;
         System.out.print(startMessage);
-        while (predicate.test(x = getNumber())) {
+        while (predicate.test(x = readNumber())) {
             System.out.print(errorMessage);
         }
         return x;
+    }
+
+    public LocalDate readDate(final String startMessage, final String errorMessage) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d.MM.yyyy");
+
+        System.out.print(startMessage);
+
+        do {
+            try {
+                String line = scanner.next();
+                return LocalDate.parse(line, formatter);
+            } catch (DateTimeParseException ex) {
+                System.out.print(errorMessage);
+            }
+        } while (true);
+    }
+
+    public long getTimeCut(Reader reader) {
+        long days;
+        do {
+            LocalDate date1 = reader.readDate("\tInput first date: ", "\tTry again: ");
+            LocalDate date2 = reader.readDate("\tInput second date: ", "\tTry again: ");
+
+            days = ChronoUnit.DAYS.between(date1, date2);
+
+            if (days < 0) {
+                System.out.println("\tTry again enter time cut!");
+            }
+        } while (days < 0);
+
+        return days;
     }
 }
